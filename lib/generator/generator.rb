574ae65badce0926e5218c7c30935825cd70fab1
Max Reznichenko <max.reznichenko@gmail.com>
2015-03-29 22:46:29 +0300

    Added better sitemap headers

--- lib/generator/generator.rb
+++ lib/generator/generator.rb
@@ -52,7 +52,10 @@ module Generator
 
     xml = Builder::XmlMarkup.new(target: output_io, indent: 2)
     xml.instruct!
-    xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do
+    xml.urlset(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9",
+           "xmlns:xsi"=> "http://www.w3.org/2001/XMLSchema-instance",
+           "xsi:schemaLocation"=>"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+      ) do
       urls.each do |url_object|
         xml.url do
           xml.loc url_object.href
@@ -80,7 +83,10 @@ module Generator
 
     xml = Builder::XmlMarkup.new(target: output_io, indent: 2)
     xml.instruct!
-    xml.sitemapindex(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do
+    xml.sitemapindex(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9",
+           "xmlns:xsi"=> "http://www.w3.org/2001/XMLSchema-instance",
+           "xsi:schemaLocation"=>"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+      ) do
       sitemaps.each do |sitemap|
         xml.sitemap do
           xml.loc "http://example.com/#{sitemap}"
