487a65233ab148c3f205c7bfa6f791d71930493f
Max Reznichenko <max.reznichenko@gmail.com>
2015-03-29 21:48:12 +0300

    Revert services logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,6 @@ apt-get update
 apt-get install -y curl git gcc redis-server
 
 # first try git clone
-cd /opt
-git clone https://Ser1al@github.com/Ser1aL/uawc
-cd uawc
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
@@ -16,6 +13,10 @@ echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
 rvm install ruby-2.2.1
 rvm use ruby-2.2.1@uawc --default --create
 
+cd /opt
+git clone https://Ser1al@github.com/Ser1aL/uawc
+cd uawc
+
 bundle install
 
 echo 'Starting services'
@@ -23,3 +24,4 @@ cd /opt/uawc && ./restart_server.sh
 echo 'Puma started!'
 cd /opt/uawc && ./restart_resque.sh
 echo 'Resque started!'
+
