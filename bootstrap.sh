dda7f4ff494729d3be2f6f15b0045d5f48157162
Max Reznichenko <max.reznichenko@gmail.com>
2015-03-29 22:02:15 +0300

    First clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@ apt-get update
 apt-get install -y curl git gcc redis-server
 
 # first try git clone
+cd /opt
+git clone https://Ser1al@github.com/Ser1aL/uawc
+cd uawc
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
@@ -12,10 +15,6 @@ echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
 
 rvm install ruby-2.2.1
 rvm use ruby-2.2.1@uawc --default --create
-
-cd /opt
-git clone https://Ser1al@github.com/Ser1aL/uawc
-cd uawc
 
 bundle install
 
