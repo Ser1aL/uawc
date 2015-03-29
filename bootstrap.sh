314e5d57184e82382ea005c160bcbe99e951849c
Max Reznichenko <max.reznichenko@gmail.com>
2015-03-29 21:15:30 +0300

    Workaround github HTTPs issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@
 apt-get update
 apt-get install -y curl git gcc redis-server
 
+# first try git clone
+cd /opt
+git clone https://Ser1al@github.com/Ser1aL/uawc
+cd uawc
+
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
 source /usr/local/rvm/scripts/rvm
@@ -10,11 +15,6 @@ echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
 
 rvm install ruby-2.2.1
 rvm use ruby-2.2.1@uawc --default --create
-
-mkdir /opt -p
-cd /opt
-git clone https://github.com/Ser1aL/uawc
-cd uawc
 
 bundle install
 
