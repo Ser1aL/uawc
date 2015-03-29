4e4e8e8042fb92880f91c23ab290c4abacd21c20
Max Reznichenko <max.reznichenko@gmail.com>
2015-03-29 19:56:57 +0300

    Fixed startup scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ apt-get install -y curl git gcc redis-server
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
 source /usr/local/rvm/scripts/rvm
+echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
 
 rvm install ruby-2.2.1
 rvm use ruby-2.2.1@uawc --default --create
@@ -17,5 +18,5 @@ cd uawc
 
 bundle
 
-./restart-server
-./restart-resque
+./restart-server.sh
+./restart-resque.sh
